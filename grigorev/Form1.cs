d96bcafea1d738c651e67dd4389915722a8af25a
Григорьев(Родькин) <157117864+xew50@users.noreply.github.com>
2025-12-09 13:18:02 +0300

    Update Form1.cs

--- grigorev/Form1.cs
+++ grigorev/Form1.cs
@@ -1,4 +1,4 @@
-﻿using ghuschyan3rkis.DB;
+﻿using grigorev.DB;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -10,7 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ghuschyan3rkis
+namespace grigorev
 {
     public partial class Form1 : Form
     {
